ec6b7dd78f0045d3bb01eb06bfc46579c8a1eb4a
Purdy, Michael Shawn <purdy@utk.edu>
2022-09-19 13:42:52 -0400

    Established way of creating patterns. Set up theme.json to do some rudimetary styling

--- src/functions.php
+++ src/functions.php
@@ -11,3 +11,30 @@
 // UT Filters below
 require_once ( 'filters/region-headsearch.php' );
 require_once ( 'filters/region-footer.php' );
+
+
+
+/**
+ * Registers block categories, and type.
+ *
+ * (Stole this from utkchancellor)
+ */
+function utkchancellor_register_block_pattern_categories() {
+
+	/* Functionality specific to the Block Pattern Explorer plugin. */
+	if ( function_exists( 'register_block_pattern_category_type' ) ) {
+		register_block_pattern_category_type( 'utkchancellor', array( 'label' => __( 'utkchancellor', 'utkchancellor' ) ) );
+	}
+
+	$block_pattern_categories = array(
+		'utkchancellor-general' => array(
+			'label'         => __( 'General', 'utkchancellor' ),
+			'categoryTypes' => array( 'utkchancellor' ),
+		),
+	);
+
+	foreach ( $block_pattern_categories as $name => $properties ) {
+		register_block_pattern_category( $name, $properties );
+	}
+}
+add_action( 'init', 'utkchancellor_register_block_pattern_categories', 9 );
