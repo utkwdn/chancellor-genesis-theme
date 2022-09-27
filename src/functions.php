d8fbc2d559a8eb647289fb89e09832b0b5f2503d
Purdy, Michael Shawn <purdy@utk.edu>
2022-09-27 14:27:23 -0400

    Re-add layout options for archive pages.

--- src/functions.php
+++ src/functions.php
@@ -61,18 +61,12 @@ function authority_register_grid_layout() {
 		'authority-grid', // A layout slug of your choice. Used in body classes. 
 		[
 			'label' => __( 'Three-column Grid', 'authority-pro' ),
-			'img'   => get_stylesheet_directory_uri() . '/images/grid.gif',
+			'img'   => get_stylesheet_directory_uri() . '/images/max-width-thumb.gif',
 			'type'  => [ 'category', 'post_tag' ],
 		]
 	);
 }
 
-
-//* Register new, custom layout
-genesis_register_layout( 'content-bottom-sidebars', array(
-    'label' => 'Max Width, No Sidebars',
-    'img' => get_stylesheet_directory_uri() . '/images/content-bottom-sidebars.png',
-) );
 
 //* Include template
 add_action( 'template_include', 'content_bottom_sidebars_template', 9999 );
@@ -89,6 +83,15 @@ function content_bottom_sidebars_template( $template ) {
 	return $template;
 
 }
+
+// Add layout options back in on archive pages
+if ( function_exists( 'genesis_add_type_to_layout' ) ) {
+	genesis_add_type_to_layout( 'content-sidebar', [ 'category', 'post_tag' ] );
+	genesis_add_type_to_layout( 'sidebar-content', [ 'category', 'post_tag' ] );
+	genesis_add_type_to_layout( 'full-width-content', [ 'category', 'post_tag' ] );
+}
+
+
 
 
 // Kill the two side-bar layouts
